6d916ecf0fe0f7acf3938d3d9a412326478449c5
Tek Bhattarai <tekbhattarai5@gmail.com>
2023-01-24 11:25:19 -0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.png';
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">
+          <img src={logo} alt="Logo" />
+        </a>
+      </header>
+      <maim> this is main content</maim>
+    </div>
+  );
+}
+
+export default App;
